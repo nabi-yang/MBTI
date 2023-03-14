8e07e5a11b3eaf1984393aa3e04b63afb0944a34
nabi <sunae894@naver.com>
2023-03-14 15:44:56 +0900

    ver02

---
+++ js/questions.js
@@ -0,0 +1,45 @@
+import {questions} from './data.js'  /*데이터 가져올때 */ 
+
+const progressValueEl = document.querySelector('.progress .value');
+const numberEl = document.querySelector('.number');
+const questionEl = document.querySelector('.question');
+const choice1El = document.querySelector('.choice1');
+const choice2El = document.querySelector('.choice2');
+
+
+let currentNumber = 0 ;
+let mbti = '';
+
+function renderQuestion() {
+    if(currentNumber === questions.length-1 ){ //9를 쓰는것보다 이렇게적는게 더좋다
+        showResultPage()
+        return
+    }
+    const question = questions[currentNumber]
+    numberEl.innerHTML = question.number
+    questionEl.innerHTML = question.question
+    choice1El.innerHTML = question.choices[0].text
+    choice2El.innerHTML = question.choices[1].text
+    progressValueEl.style.width = (currentNumber +1) *10 + '%'
+}
+
+function nextQuestion(choiceNumber) { /**/
+    const question = questions[currentNumber];
+    mbti = mbti + question.choices[choiceNumber].value
+    currentNumber=currentNumber+1
+    renderQuestion()
+}
+
+function showResultPage() { //결과페이지
+    location.href = '/results.html?mbti=' + mbti //쿼리스트링 
+}
+
+choice1El.addEventListener('click', function() {
+    nextQuestion(0)
+})
+
+choice2El.addEventListener('click', function() {
+    nextQuestion(1)
+})
+
+renderQuestion()
